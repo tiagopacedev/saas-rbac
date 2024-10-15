74faf0a756323ae99be9cd9f05450db64d1c774e
Tiago Pace <tiagopace.dev@gmail.com>
2024-10-15 16:34:14 -0300

    feat(auth): create initial permissions setup

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElese = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElese)
+console.log(userCanDeleteOtherUsers)
+console.log(userCannotDeleteOtherUsers)
