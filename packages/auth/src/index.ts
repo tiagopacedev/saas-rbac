894e96fd8bec6c1ffd0bfd88773aaecd2e36b635
Tiago Pace <tiagopace.dev@gmail.com>
2024-10-15 17:50:13 -0300

    feat(auth): define application permissions

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -13,6 +13,10 @@ import { organizationSubject } from './subjects/organization'
 import { inviteSubject } from './subjects/invite'
 import { billingSubject } from './subjects/billing'
 
+export * from './models/organization'
+export * from './models/project'
+export * from './models/user'
+
 const appAbilitiesSchema = z.union([
   projectSubject,
   userSubject,
